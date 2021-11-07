35cb83d25c8a0fe5004945369ec2748228d553de
SatYu26 <goyalsatyam8@gmail.com>
2021-11-08 00:45:00 +0530

    adding sorting and fields filter query

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,40 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    page: "2",
-  });
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  // fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
